8ea2d119f7c211b53d89b7a21f4a8dad0e9a359e
pedroLSoares <pedro.levada@mercadolivre.com>
2022-03-17 16:31:50 -0300

    Add morse conversor

--- src/main/java/com/pedrolsoares/itbootcampw5spring/conversor/controller/ConversorController.java
+++ src/main/java/com/pedrolsoares/itbootcampw5spring/conversor/controller/ConversorController.java
@@ -1,22 +1,28 @@
 package com.pedrolsoares.itbootcampw5spring.conversor.controller;
 
+import com.pedrolsoares.itbootcampw5spring.conversor.service.MorseConversorService;
 import com.pedrolsoares.itbootcampw5spring.conversor.service.RomanConversorService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/v1/conversor")
 public class ConversorController {
 
     private final RomanConversorService romanConversorService;
+    private final MorseConversorService morseConversorService;
 
-    @Autowired
-    public ConversorController(RomanConversorService romanConversorService) {
-        this.romanConversorService = romanConversorService;
-    }
 
-    @GetMapping("/{number}")
+    @GetMapping("/roman/{number}")
     public String convertRomanNumber(@PathVariable int number){
         return romanConversorService.convertRomanNumber(number);
     }
+
+    @GetMapping("/morse/{morse}")
+    public String convertRomanNumber(@PathVariable String morse){
+        return morseConversorService.convertMorseCode(morse);
+    }
+
 }
--- src/main/java/com/pedrolsoares/itbootcampw5spring/conversor/service/MorseConversorService.java
+++ src/main/java/com/pedrolsoares/itbootcampw5spring/conversor/service/MorseConversorService.java
@@ -11,15 +11,59 @@ public class MorseConversorService {
     public String convertMorseCode(String morse){
         Map<String,String> characterMap = new HashMap<>();
         characterMap.put("._", "A");
-        characterMap.put("._", "B");
-        characterMap.put("._", "C");
-        characterMap.put("._", "D");
-        characterMap.put("._", "E");
-        characterMap.put("._", "F");
-        characterMap.put("._", "G");
-        characterMap.put("._", "H");
+        characterMap.put("_...", "B");
+        characterMap.put("_._.", "C");
+        characterMap.put("_..", "D");
+        characterMap.put(".", "E");
+        characterMap.put(".._.", "F");
+        characterMap.put("__.", "G");
+        characterMap.put("....", "H");
+        characterMap.put("..", "I");
+        characterMap.put(".___", "J");
+        characterMap.put("_._", "K");
+        characterMap.put("._..", "L");
+        characterMap.put("__", "M");
+        characterMap.put("_.", "N");
+        characterMap.put("___", "O");
+        characterMap.put(".__.", "P");
+        characterMap.put("__._", "Q");
+        characterMap.put("._.", "R");
+        characterMap.put("...", "S");
+        characterMap.put("_", "T");
+        characterMap.put(".._", "U");
+        characterMap.put("..._", "V");
+        characterMap.put(".__", "W");
+        characterMap.put("_.._", "X");
+        characterMap.put("_.__", "Y");
+        characterMap.put("__..", "Z");
+        characterMap.put("_____", "0");
+        characterMap.put(".____", "1");
+        characterMap.put("..___", "2");
+        characterMap.put("...__", "3");
+        characterMap.put("...._", "4");
+        characterMap.put(".....", "5");
+        characterMap.put("_....", "6");
+        characterMap.put("__...", "7");
+        characterMap.put("___..", "8");
+        characterMap.put("____.", "9");
+        characterMap.put("..__..", "?");
+        characterMap.put("_._.__", "!");
+        characterMap.put("._._._", ".");
+        characterMap.put("__..__", ",");
 
-        return  "";
+        String[] splittedMorse = morse.split(" ");
+
+        StringBuilder word = new StringBuilder();
+        for(String code:splittedMorse){
+            if(code.isEmpty()){
+                word.append(" ");
+                continue;
+            }
+            String charac = characterMap.get(code);
+            word.append(charac);
+        }
+
+        return  word.toString();
 
     }
 }
