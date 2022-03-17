1f6fdfa03e171d6aade6d30b4fd6b397fafe09ca
pedroLSoares <pedro.levada@mercadolivre.com>
2022-03-17 15:24:33 -0300

    First commit - Roman numbers conversor

---
+++ src/main/java/com/pedrolsoares/itbootcampw5spring/ItBootcampW5SpringApplication.java
@@ -0,0 +1,13 @@
+package com.pedrolsoares.itbootcampw5spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItBootcampW5SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItBootcampW5SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pedrolsoares/itbootcampw5spring/ItBootcampW5SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrolsoares.itbootcampw5spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItBootcampW5SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
